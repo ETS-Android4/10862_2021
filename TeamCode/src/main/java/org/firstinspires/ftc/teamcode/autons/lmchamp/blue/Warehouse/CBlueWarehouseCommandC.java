7cf39fe8c63e8fe6074219e7e00672e1d5a7add8
Lucky <saivarshini.chinnasani@gmail.com>
2021-12-29 16:05:03 -0600

    Added Intake Command to Red Warehouse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autons/lmchamp/blue/Warehouse/CBlueWarehouseCommandC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autons/lmchamp/blue/Warehouse/CBlueWarehouseCommandC.java
@@ -41,6 +41,29 @@ public class CBlueWarehouseCommandC extends SequentialCommandGroup {
                 new DriveForwardCommand(drivetrain, 22),
                 new TurnCommand(drivetrain,90),
                 new DriveForwardCommand(drivetrain, -40)
+
+                //intake
+                new IntakeCommand(lift, intake),
+                new ColorIntakeCommand(lift, intake, colorSensor),
+
+                new DriveForwardCommand(drivetrain, 40),
+                new TurnToCommand(drivetrain, 0),
+                new DriveForwardCommand(drivetrain, -24),
+                new TurnToCommand(drivetrain, 298),
+                new ArmMidCommand(lift),
+                new WaitCommand(1000),
+
+                new KindaSlowDriveForwardCommand(drivetrain, -4.5),
+                new DropCommand(armServos),
+                new KindaSlowDriveForwardCommand(drivetrain, -0.5),
+                new WaitCommand(1000),
+                new InstantCommand(armServos::armUp,armServos),
+
+                new TurnToCommand(drivetrain, 0, true),
+                new ResetCommand(armServos, lift),
+                new DriveForwardCommand(drivetrain, 22),
+                new TurnCommand(drivetrain,90),
+                new DriveForwardCommand(drivetrain, -40)
         );
     }
 }
