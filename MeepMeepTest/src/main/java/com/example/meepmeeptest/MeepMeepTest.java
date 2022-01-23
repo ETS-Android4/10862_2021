8d1d448f9b6c915d4f065c068bf5e61a1efd0a07
Lucky <saivarshini.chinnasani@gmail.com>
2022-01-23 14:57:51 -0600

    Meep Meep Path for Red Warehouse

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptest;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,17 +12,29 @@ public class MeepMeepTest {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(55, 55, Math.toRadians(300.704976), Math.toRadians(300.704976), 14.5)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                                .splineTo(new Vector2d(20,25), 0)
+                                .splineTo(new Vector2d(-8,-35), Math.toRadians(267))
+                                .forward(5)
+                                .splineTo(new Vector2d(20,25.5), Math.toRadians(0))
+                                .splineTo(new Vector2d(-8,-35), Math.toRadians(270))
+                                .forward(5)
+                                .splineTo(new Vector2d(20,25.5), Math.toRadians(0))
+                                .splineTo(new Vector2d(-8,-35), Math.toRadians(270))
+                                .forward(5)
+                                .splineTo(new Vector2d(20,25.5), Math.toRadians(0))
+                                .splineTo(new Vector2d(-9,-35), Math.toRadians(270))
+
+                                /*splineTo(new Vector2d(10, 10), 0)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .splineTo(new Vector2d(25, -15), 0)
+                                .waitSeconds(3)
+                                .turn(Math.toRadians(45))
+                                .forward(10)
+                                .strafeRight(5)
+                                .turn(Math.toRadians(90))*/
                                 .build()
                 );
 
