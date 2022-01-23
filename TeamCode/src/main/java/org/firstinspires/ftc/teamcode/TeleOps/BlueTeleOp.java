8f1971c15111c7f576db45e1ba456114ea3dd3ac
Lucky <saivarshini.chinnasani@gmail.com>
2022-01-23 17:07:45 -0600

    Moves TeleOps into folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BlueTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/BlueTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.TeleOps;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.button.Button;
